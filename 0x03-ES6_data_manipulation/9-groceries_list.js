bd7564db3c9c5869ea12bb4dcd44ee53c84f7d6b
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-15 17:57:12 +0100

    add files

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,6 +1,6 @@
 const updateUniqueItems = (items) => {
   if (!(items instanceof Map)) {
-    throw new Error('Canot process');
+    throw new Error('Cannot process');
   }
   for (const [k, v] of items.entries()) {
     if (v === 1) {
