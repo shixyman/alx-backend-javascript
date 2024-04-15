ad652b56cf3b8a35f622f835b4b3f882cfc4915c
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-15 14:55:45 +0100

    add files

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,5 +5,7 @@ function updateStudentGradeByCity(students, city, newGrades) {
       const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
       const grade = gradeObj ? gradeObj.grade : 'N/A';
       return { ...student, grade };
-  });
+    });
 }
+
+export default updateStudentGradeByCity;
