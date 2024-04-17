e930e6fce347d589fb487ef318c1b1a0cf34929b
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-17 23:01:12 +0100

    modify

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,13 +1,46 @@
-// Define the isDirector type predicate function
-function isDirector(employee: Director | Teacher): employee is Director {
-    return (employee as Director).workDirectorTasks !== undefined;
+interface Teacher {
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
+    location: string,
+    [propName: string]: any,
 }
 
-// Define the executeWork function
-function executeWork(employee: Director | Teacher): string {
-    if (isDirector(employee)) {
-        return employee.workDirectorTasks();
-    } else {
-        return employee.workTeacherTasks();
+interface Directors extends Teacher {
+    numberOfReports: Number,
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName.charAt(0)}.${lastName}`;
+interface classInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements classInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName;
+        this.lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+    displayName(): string {
+        return this.firstName;
     }
 }
+interface StudentConstructor {
+    (firstName: string, lastName: string): classInterface;
+}
+
+const student = new StudentClass("Djo", "djo");
+console.log(student.displayName())
+console.log(student.workOnHomework())
