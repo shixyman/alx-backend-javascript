8079a07289819b827191b83ac4d086503844d40e
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-18 01:53:04 +0100

    modify

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,17 +1,26 @@
 interface MajorCredits {
     credits: number;
-    brand?: string;
 }
 
 interface MinorCredits {
     credits: number;
-    brand: string;
 }
 
-export const sumMajorCredits = (subject1: MajorCredits, subject2: MajorCredits): number => {
-    return subject1.credits + subject2.credits;
+interface Major extends MajorCredits {
+    credits: number;
 }
 
-export const sumMinorCredits = (subject1: MinorCredits, subject2: MinorCredits): number => {
-    return subject1.credits + subject2.credits;
+interface Minor extends MinorCredits {
+    credits: number;
+}
+
+function sumMajorCredits(subject1: Major, subject2: Major): Major {
+    return {
+        credits: subject1.credits + subject2.credits
+    }
+}
+function sumMinorCredits(subject1: Minor, subject2: Minor): Minor {
+    return {
+        credits: subject1.credits + subject2.credits
+    }
 }
