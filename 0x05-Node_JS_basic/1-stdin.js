c958dec3e2c788fbcdc896b70c54334bf0b098a8
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-06-03 13:13:25 +0100

    1. Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data.toString().trim()}`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
