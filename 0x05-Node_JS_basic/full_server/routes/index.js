14023f6174d09244b19e234021d06779c73969ea
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-06-04 14:35:04 +0100

    modify15

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const AppController = require('../controllers/AppController');
-const StudentsController = require('../controllers/StudentsController');
+import express from 'express';
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentController';
 
-const router = express.Router();
+function controllerRouting(app) {
+  const router = express.Router();
+  app.use('/', router);
 
-router.get('/', AppController.getHomepage);
+  router.get('/', (req, res) => {
+    AppController.getHomepage(req, res);
+  });
 
-router.get('/students', StudentsController.getAllStudents);
+  router.get('/students', (req, res) => {
+    StudentsController.getAllStudents(req, res, process.argv[2]);
+  });
 
-router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+  router.get('/students/:major', (req, res) => {
+    StudentsController.getAllStudentsByMajor(req, res, process.argv[2]);
+  });
+}
 
-module.exports = router;
+module.exports = controllerRouting;
