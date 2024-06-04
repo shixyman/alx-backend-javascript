55a82ab34489c5406389588aad86a16ac027deb8
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-06-04 14:12:45 +0100

    modify11

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,14 +1,12 @@
-const express = require('express');
-
-const router = require('./routes/index');
+import express from 'express';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = 1245;
 
-app.use('/', router);
-app.use('/students', router);
-app.use('/students/:major', router);
+controllerRouting(app);
 
-app.listen(port);
-
-export default app;
+app.listen(port, () => {
+  console.log(`Express Server app listening at http://localhost:${port}`);
+});
+module.exports = app;
