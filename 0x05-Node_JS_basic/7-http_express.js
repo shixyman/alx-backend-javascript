26dd0fedd18301080075f6bd1ed29d65c9597767
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-06-04 13:45:36 +0100

    modify5

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,42 +1,31 @@
 #!/usr/bin/node
 
 const express = require('express');
-const fs = require('fs');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
 
 const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  const filename = process.argv[2];
-
-  fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send('Error reading file');
-      return;
-    }
-
-    const lines = data.trim().split('\n');
-    const students = lines.filter((line) => line.trim() !== '').map((line) => line.split(','));
-    const numStudents = students.length;
-
-    let response = 'This is the list of our students\n';
-    response += `Number of students: ${numStudents}\n`;
-    response += 'First name | Last name | Score\n';
-    response += '--------- | --------- | -----\n';
-
-    for (const [firstName, lastName, score] of students) {
-      response += `${firstName} | ${lastName} | ${score}\n`;
-    }
-
-    res.send(response);
-  });
+app.get('/students', async (req, res) => {
+  const message = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DATABASE);
+    res.end(`${message}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${message}${error.message}`);
+  }
 });
 
-app.listen(1245, () => {
-  console.log('Server running at http://localhost:1245/');
+app.listen(port, () => {
+  console.log(`Express Server running at http://localhost:${port}/`);
 });
 
 module.exports = app;
