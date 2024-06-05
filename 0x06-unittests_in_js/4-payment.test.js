35707c95c1688c46ad6ea5f74adad306e594c7eb
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-06-05 16:55:35 +0100

    add files task 3

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
+
+describe('sendPaymentRequestToApi', function() {
+  let utilsStub;
+  let consoleLogSpy;
+
+  beforeEach(function() {
+    utilsStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function() {
+    utilsStub.restore();
+    consoleLogSpy.restore();
+  });
+
+  it('should call Utils.calculateNumber with the right arguments and log the correct message', function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(utilsStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+  });
+});
